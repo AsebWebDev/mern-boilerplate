a0988dab7769e9e0acaa0013fecd3d98863cb2df
André Sebastian <a.sebastian@posteo.de>
2019-03-09 16:01:38 +0100

    First Commit

---
+++ server/bin/seeds.js
@@ -0,0 +1,35 @@
+const path = require('path')
+require('dotenv').config({ path: path.join(__dirname, '../.env') })
+
+const mongoose = require("mongoose");
+// const User = require("../models/User");
+
+require('../configs/database')
+
+let users = [
+  {
+    username: "alice",
+    password: bcrypt.hashSync("alice", bcrypt.genSaltSync(bcryptSalt)),
+  },
+  {
+    username: "bob",
+    password: bcrypt.hashSync("bob", bcrypt.genSaltSync(bcryptSalt)),
+  }
+]
+
+User.deleteMany()
+  .then(() => {
+    return User.create(users)
+  })
+  .then(usersCreated => {
+    console.log(`${usersCreated.length} users created with the following id:`);
+    console.log(usersCreated.map(u => u._id));
+  })
+  .then(() => {
+    // Close properly the connection to Mongoose
+    mongoose.disconnect()
+  })
+  .catch(err => {
+    mongoose.disconnect()
+    throw err
+  })
